ad138a95c7cb45c1ed4177e24ff67925a47d816c
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2020-11-17 21:30:01 +0100

    3.1

--- java.js
+++ java.js
@@ -1,3 +1,5 @@
+var audio = new Audio('music/aerohead-path-of-the-fireflies.mp3');
+
 function insert(num){
     document.form.textview.value=document.form.textview.value+num
 }
@@ -17,3 +19,9 @@ function back(){
     var exp=document.form.textview.value;
     document.form.textview.value=exp.substring(0,exp.length-1);
 }
+
+function play1(){
+audio.volume = 0.03
+audio.play();
+}
+
